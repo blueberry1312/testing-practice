c90fd98552cbbae216c2c1781a9dcb94f3fa3e42
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-03-01 10:40:58 -0300

    Add stringLength test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const stringLength = require('./index')
+
+test('should be print is 5', () => {
+    expect(stringLength('house')).toBe(5);
+});
+test('should be print "String is empty"', () => {
+    expect(stringLength('')).toBe('String is empty');
+});
+test('should be print is "String is to long"', () => {
+    expect(stringLength('house of carts blue')).toBe('String is to long');
+});
