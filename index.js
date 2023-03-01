b0b920c719ac62c8e595e8f07b8711c9da50e0e4
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-03-01 10:43:28 -0300

    Add reverseString test

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const stringLength = (string) => {
-    numCharacter = string.split('').length;
-    if (numCharacter < 1) {
-      return 'String is empty';
-    }
-    if (numCharacter > 10) {
-      return 'String is to long';
-    }
-return numCharacter;
+  numCharacter = string.split('').length;
+  if (numCharacter < 1) {
+    return 'String is empty';
+  }
+  if (numCharacter > 10) {
+    return 'String is to long';
+  }
+  return numCharacter;
 };
 
-module.exports = stringLength;
+const reverseString = (string) => {
+  return string.split('').reverse().join('');
+};
+
+module.exports = { stringLength, reverseString };
--- index.test.js
+++ index.test.js
@@ -1,11 +1,15 @@
-const stringLength = require('./index')
+const { stringLength, reverseString } = require('./index')
 
 test('should be print is 5', () => {
-    expect(stringLength('house')).toBe(5);
+  expect(stringLength('house')).toBe(5);
 });
 test('should be print "String is empty"', () => {
-    expect(stringLength('')).toBe('String is empty');
+  expect(stringLength('')).toBe('String is empty');
 });
 test('should be print is "String is to long"', () => {
-    expect(stringLength('house of carts blue')).toBe('String is to long');
+  expect(stringLength('house of carts blue')).toBe('String is to long');
+});
+
+test('should be print is "esuoh"', () => {
+  expect(reverseString('house')).toBe('esuoh');
 });
